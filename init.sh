2a51291a08256a865b58f636f3b5abf1f69cf5be
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-08-30 10:02:36 +0100

    New project name is an argument to the init script.

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-PROJECT=new_project_name
-
-mv src/horse-draft.tex src/$PROJECT-draft.tex
-mv src/horse.tex src/$PROJECT.tex
+mv src/horse-draft.tex src/$1-draft.tex
+mv src/horse.tex src/$1.tex
 rm src/main.tex
 touch src/main.tex
